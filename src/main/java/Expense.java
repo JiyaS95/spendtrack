9f12de0b937e66d2d46ba8e00cb46556c62474cc
JiyaS95 <jiya.shukla@mail.utoronto.ca>
2025-08-07 19:05:38 +0000

    fixed

---
+++ src/main/java/Expense.java
@@ -0,0 +1,14 @@
+public class Expense {
+    public String date;
+    public String category;
+    public double amount;
+    public String note;
+    public Expense(String date, String category, double amount, String note) {
+        this.date = date;
+        this.category = category;
+        this.amount = amount;
+        this.note = note;
+    }
+
+
+}
--- src/main/java/SpendTrack.java
+++ src/main/java/SpendTrack.java
@@ -1,5 +1,75 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+ * Compile & Run:
+ * javac SpendTrack.java Expense.java
+ * java SpendTrack
+ */
+
 public class SpendTrack {
+    static Scanner scan = new Scanner(System.in);
+    static ArrayList<Expense> expenses = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        System.out.println("Track Your Spendings!\n\n");
+
+        int choice = 0;
+        while (choice != 3) {
+            showMenu();
+            choice = getUserChoice();
+
+            if (choice == 1) {
+                addExpense();
+            }
+
+            else if (choice == 2) {
+                viewExpenses();
+            }
+        }
+        
+    }
+
+    public static void showMenu() {
+        System.out.println("Selection Panel: \n-----------------\nAdd Expense - 1\nView all Expenses - 2\nExit - 3");
+        System.out.print("Choice: ");
+    }
+
+    public static int getUserChoice() {
+        int choice = scan.nextInt();
+        scan.nextLine();
+        return choice;
+    }
+
+    public static void addExpense() {
+        System.out.println("\nEnter the Date:");
+        String date = scan.nextLine();
+        
+        System.out.println("Enter the Category:");
+        String category = scan.nextLine();
+
+        System.out.println("Enter the Amount:");
+        Double amount = scan.nextDouble();
+        scan.nextLine();
+
+        System.out.println("Enter the Note:");
+        String note = scan.nextLine();
+        Expense exp = new Expense(date, category, amount, note);
+        expenses.add(exp);
+    }
+
+    public static void viewExpenses() {
+        if (expenses.isEmpty()) {
+            System.out.println("No expenses recorded yet.\n");
+        }
+
+        for (int i = 0; i < expenses.size(); ++i) {
+            Expense e = expenses.get(i);
+            System.out.println("Date: " + e.date);
+            System.out.println("Category: " + e.category);
+            System.out.println("Amount: $" + e.amount);
+            System.out.println("Note: " + e.note);
+            System.out.println("------------------------");
+        }
     }
 }
