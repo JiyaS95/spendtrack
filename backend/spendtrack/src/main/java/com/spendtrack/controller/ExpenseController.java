2ed0d8f276a448f18d3325a1022aa25d54081d1d
JiyaS95 <jiya.shukla@mail.utoronto.ca>
2025-12-27 01:45:23 +0000

    Update Expense entity and controller to compile and run on Java 17

--- backend/spendtrack/src/main/java/com/spendtrack/controller/ExpenseController.java
+++ backend/spendtrack/src/main/java/com/spendtrack/controller/ExpenseController.java
@@ -2,6 +2,7 @@ package com.spendtrack.controller;
 
 import com.spendtrack.entity.Expense;
 import com.spendtrack.repository.ExpenseRepository;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
